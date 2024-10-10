14bba0c622ef1904c961119e14c5b7f5a1b5cfc4
Benjamin <benjamindeltour22@gmail.com>
2024-10-10 14:16:10 +0200

    feat: add click on note icon

--- cluedo_front/src/components/molecules/Header.jsx
+++ cluedo_front/src/components/molecules/Header.jsx
@@ -2,13 +2,23 @@ import React from 'react'
 import { Text, Container, Visual } from '../atoms'
 
 const Header = ({ timer = '88:88', title = 'Salle actuelle' }) => {
+
+  const handleNoteClick = () => {
+    console.log('note clique')
+  }
+
   return (
     <Container.Row bgColor={'#fcdd62'} padding={'3px'}>
       <Container.Row bgColor={'#b59d46'} padding={'5px 15px'}>
         <Text.Paragraph fontSize={'24px'}>{timer}</Text.Paragraph>
       </Container.Row>
       <Text.Title>{title}</Text.Title>
-      <Visual.Icon src={'/assets/img/book1.svg'} width={'40px'} height={'40px'}></Visual.Icon>
+      <Visual.Icon
+        src={'/assets/img/book1.svg'}
+        width={'40px'}
+        height={'40px'}
+        onClick={handleNoteClick}
+      ></Visual.Icon>
     </Container.Row>
   )
 }
