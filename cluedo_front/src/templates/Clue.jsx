4c771917d4abf604553cc85074968c75c94663f1
Benjamin <benjamindeltour22@gmail.com>
2024-10-11 11:06:48 +0200

    feat: add clue template

---
+++ cluedo_front/src/templates/Clue.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Header, ClueList } from '../components/molecules'
+import { Container } from '../components/atoms'
+
+const Clue = () => {
+
+  const characters = [
+    {
+      name: 'John Doe',
+      role: 'Père de la victime'
+    },
+    {
+      name: 'Maryse Gregoire',
+      role: 'Soeur de la victime'
+    },
+    {
+      name: 'Gaspar Rodrigue',
+      role: 'Fiancé de la victime'
+    },
+  ]
+
+  const data = {
+    timer: '07:29',
+    currentScene: {
+      url: '/assets/img/background/cabaret.png',
+      title: 'Cabaret',
+      leftScene: {
+        title: 'street'
+      },
+      rightScene: {
+        title: 'slum'
+      }
+    },
+  }
+
+  return (
+    <Container.Column>
+      <Header title={data.currentScene.title} timer={data.timer}></Header>
+      <ClueList characters={characters}></ClueList>
+    </Container.Column>
+  )
+}
+
+export default Clue
