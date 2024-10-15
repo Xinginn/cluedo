9f57ca0f45fdc0b1b79164692d6199c509ace586
Benjamin <benjamindeltour22@gmail.com>
2024-10-15 09:46:34 +0200

    feat: add link

--- cluedo_front/src/components/molecules/CharacterList.jsx
+++ cluedo_front/src/components/molecules/CharacterList.jsx
@@ -6,14 +6,16 @@ const CharacterList = ({ characters }) => {
   return (
     <Container.Column>
       {characters.map((character, index) => (
-        <Action.Button key={index} width="100%">
-          <Container.Row justifyContent={'flex-start'} gap={'5%'}>
-            <Visual.Image src={`/assets/img/faces/face_1.png`} width={'8rem'} />
-            <Text.Name>
-              {`${character.name} - ${character.role}`}
-            </Text.Name>
-          </Container.Row>
-        </Action.Button>
+        <Action.Link to={'/characterDetails'}>
+          <Action.Button key={index} width="100%">
+            <Container.Row justifyContent={'flex-start'} gap={'5%'}>
+              <Visual.Image src={`/assets/img/faces/face_1.png`} width={'8rem'} />
+              <Text.Name>
+                {`${character.name} - ${character.role}`}
+              </Text.Name>
+            </Container.Row>
+          </Action.Button>
+        </Action.Link>
       ))}
     </Container.Column>
   )
