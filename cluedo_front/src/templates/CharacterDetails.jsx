8d44c10b18f248159124a73b0f19dde28748ff2e
Benjamin <benjamindeltour22@gmail.com>
2024-10-15 10:10:17 +0200

    feat: add hammer icon

--- cluedo_front/src/templates/CharacterDetails.jsx
+++ cluedo_front/src/templates/CharacterDetails.jsx
@@ -14,29 +14,20 @@ const CharacterDetails = () => {
     console.log("TODO Click on denounce")
   }
 
-  const data = {
-    character: {
-      name: "John Doe",
-      bodyPicture: "/assets/img/bodies/body_1.png",
-      role: "Frère de la victime",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    }
-  }
-
   return (
     <Container.Column justifyContent="space-between" height="100vh">
       <Header title="Dossier du suspect" icon="close"></Header>
       <Container.Row justifyContent="flex-end" >
         <Action.Button width="60px" height="60px">
           <Visual.Icon
-            src={'/assets/img/icons/close-circle.svg'}
+            src={'/assets/img/icons/hammer-law.svg'}
             width={'40px'}
             height={'40px'}
-            onClick={handleDenounceClick}/>
+            onClick={handleDenounceClick} />
         </Action.Button>
       </Container.Row>
       <Container.Row>
-        <CharacterInfos character={character}/>
+        <CharacterInfos character={character} />
       </Container.Row>
       <Visual.Background url="/assets/img/backgrounds/notes.jpg" />
     </Container.Column>
