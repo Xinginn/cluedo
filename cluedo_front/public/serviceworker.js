4d096099bb6c8b94a362a9710d9cbf5fe8cb9e98
Benjamin <benjamindeltour22@gmail.com>
2024-12-01 14:01:35 +0100

    feat: improve offline respond

--- cluedo_front/public/serviceworker.js
+++ cluedo_front/public/serviceworker.js
@@ -1,39 +1,54 @@
-const CACHE_NAME = "version-1"
-const urlsToCach = ['index.html', 'offline.html']
+const CACHE_NAME = "version-1";
+const urlsToCache = ['index.html', 'offline.html'];
 
-const self = this
+const self = this;
 
+// Lors de l'installation du Service Worker
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Opened cache')
-        return cache.addAll(urlsToCach)
-      })
-  )
-})
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(() => {
-        return fetch(event.request)
-          .catch(() => caches.match('offline.html'))
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          return response;
+        })
+        .catch(() => {
+          return caches.match('offline.html');
+        })
+    );
+  } else {
+    // Pour les autres requêtes
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
       })
-  )
-})
+    );
+  }
+});
 
+
+// Activation du Service Worker
 self.addEventListener('activate', (event) => {
-  const cacheWhiteList = []
-  cacheWhiteList.push(CACHE_NAME)
+  const cacheWhiteList = [CACHE_NAME];
 
   event.waitUntil(
-    caches.keys().then((cacheNames) => Promise.all(
-      cacheNames.map((cacheName) => {
-        if (!cacheWhiteList.includes(cacheName)) {
-          return caches.delete(cacheName)
-        }
-      })
-    ))
-  )
-})
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhiteList.includes(cacheName)) {
+            console.log(`Deleting cache: ${cacheName}`);
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
--- cluedo_front/src/components/molecules/CharacterSpeech.jsx
+++ cluedo_front/src/components/molecules/CharacterSpeech.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useRef, useEffect, forwardRef } from 'react'
+import React, { useContext, useRef, useEffect, useState } from 'react'
 import { Container, Text, Visual } from '../atoms'
 import { AlternativeThemeProviderContext } from '../../provider/AlternativeThemeProvider'
 import { useSelector } from 'react-redux'
@@ -7,11 +7,11 @@ import { ScrollTarget } from '../atoms/Container'
 const CharacterSpeech = ({ character }) => {
 
   const currentCharacterId = useSelector((state) => state.currentCharacterHistorySlice.currentCharacter.id)
-
   const discussions = useSelector((state) => {
     const character = state.investigationHistorySlice.investigation.characters.find(item => item.id === currentCharacterId)
     return character.discussions
   })
+  const [isOnline, setIsOnline] = useState(true)
 
   const { whichTheme } = useContext(AlternativeThemeProviderContext)
 
@@ -22,19 +22,37 @@ const CharacterSpeech = ({ character }) => {
   }
 
   useEffect(() => {
+    const handleOnline = () => setIsOnline(true)
+    const handleOffline = () => setIsOnline(false)
+
+    setIsOnline(navigator.onLine)
+
+    window.addEventListener('online', handleOnline)
+    window.addEventListener('offline', handleOffline)
+
+    return () => {
+      window.removeEventListener('online', handleOnline)
+      window.removeEventListener('offline', handleOffline)
+    }
+  }, [isOnline])
+
+  useEffect(() => {
     scrollToBottom()
-  }, [discussions]);
+  }, [discussions])
 
   return (
     <Container.Row bgColor={'transparent'}>
       <Visual.Image height="600px" src={`/assets/img/${whichTheme.slug}/bodies/${character.gender}/${character.body}`}></Visual.Image>
       <Container.Column width="400px" maxHeight="600px" overflowY="auto" bgColor="transparent" >
+        {
+          !isOnline &&
+          <Text.Paragraph width='80%' padding="4px" borderRadius="8px" alignSelf='flex-start' bgColor="rgba(200,200,200,0.8)">Veuillez vous connecter à internet</Text.Paragraph>
+        }
         {discussions.length > 0 &&
           discussions.map((item, index) => {
             return (
               <Container.Column key={index} bgColor={'transparent'} padding="4px">
                 <Text.Paragraph width='80%' padding="4px" borderRadius="8px" alignSelf='flex-end' bgColor="rgba(200,200,200,0.8)" >{item.prompt}</Text.Paragraph>
-
                 {!!item.answer ?
                   <Text.Paragraph width='80%' padding="4px" borderRadius="8px" alignSelf='flex-start' bgColor="rgba(200,200,200,0.8)">{item.answer}</Text.Paragraph>
                   :
